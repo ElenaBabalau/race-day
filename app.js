4762ed0acaa88673ffe4996eec4b5223bee4d0a6
Elena <82116523+ElenaBabalau@users.noreply.github.com>
2021-06-30 13:24:52 +0300

    Added app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+function raceTime(userAge, registredEarly) {
+    console.log(userAge, registredEarly);
+}
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const earlyRegistered = true;
+
+let runnerAge = 23;
+
+if(earlyRegistered === true) {
+  raceNumber += 1000;
+}
+
+if(runnerAge >= 18 && raceNumber < 1000){
+  console.log(`You will race at 9:30am and your race number is ${raceNumber}`);
+}
+else if(runnerAge >= 18 && raceNumber < 1000){
+  console.log(`You will race at 11:00am and your race number is ${raceNumber}`);
+}
+else if(runnerAge <= 18 || raceNumber >= 1000){
+  console.log(`You will race at 12:30am and your race number is ${raceNumber}`);
+} 
+
+raceTime(23, true);
